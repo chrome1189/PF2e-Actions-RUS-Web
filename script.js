7c98660e9947b3c8b165ed5c89059d890f27a074
Max <chrome1189@gmail.com>
2024-06-26 22:02:45 +0300

    фыв

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 var modalwin = document.getElementById('modalwin');
-
-function openCommon () {
-    modalwin.style.display = 'flex';
-};
+var actCommon = document.getElementById('common');
+var actAcro = document.getElementById('acrobatics');
 
 function closeAllWin () {
     modalwin.style.display = 'none';
+    actCommon.style.display = 'none';
+    actAcro.style.display = 'none';
+};
+
+// Модальные окна
+
+function openCommon () {
+    modalwin.style.display = 'flex';
+    actCommon.style.display = 'block';
+};
+
+function openAcro () {
+    modalwin.style.display = 'flex';
+    actAcro.style.display = 'block';
 };
